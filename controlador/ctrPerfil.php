5fa08b7abf06039e004ecd602ddf294b7a669fa7
YANITZINMARTINEZBASILIO <151480753+YANITZINMARTINEZBASILIO@users.noreply.github.com>
2024-02-04 18:43:18 -0600

    Add files via upload

---
+++ controlador/ctrPerfil.php
@@ -0,0 +1,11 @@
+<?php
+    class Perfil{
+        static public function ctrPerfil(){
+                include "vista/perfil.php";
+        }
+
+        static public function ctrRuta(){
+            return "http://localhost:8080/C'MON/vista/recursos";
+        }
+    }
+?>
---
+++ controlador/ctrPlantilla.php
@@ -0,0 +1,12 @@
+<?php
+    class Plantilla{
+        static public function ctrPlantilla(){
+                include "vista/plantilla.php";
+        }
+        
+
+        static public function ctrRuta(){
+            return "http://localhost:8080/C'MON/vista/recursos";
+        }
+    }
+?>
