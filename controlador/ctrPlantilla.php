e474bffeca8b5f4c4e8786abf115dabb6f2ae248
YANITZINMARTINEZBASILIO <233271020@alumnos.utn.edu.mx>
2024-02-06 17:37:32 -0600

    pestañasCommit

--- controlador/ctrPlantilla.php
+++ controlador/ctrPlantilla.php
@@ -4,7 +4,11 @@
                 include "vista/plantilla.php";
         }
         
+        
+    }
 
+
+    class Ruta{
         static public function ctrRuta(){
             return "http://localhost:8080/C'MON/vista/recursos";
         }
